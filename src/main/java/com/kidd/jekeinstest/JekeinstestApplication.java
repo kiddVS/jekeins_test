fd4347d292180bdc7d9f9f55537574bb3649d0ea
jiangchang1 <jiangchang1@yiche.com>
2018-09-21 17:12:13 +0800

    feat:init project

---
+++ src/main/java/com/kidd/jekeinstest/JekeinstestApplication.java
@@ -0,0 +1,12 @@
+package com.kidd.jekeinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JekeinstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JekeinstestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kidd/jekeinstest/JekeinstestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kidd.jekeinstest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JekeinstestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
